bb2909eb453483b2a9c86a896cf9c7b0e9da45d4
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2023-12-28 15:48:23 +0200

    Add cookie consent bar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,22 @@
 import './Footer.css';
+import CookieConsent from "react-cookie-consent";
+
 
 function Footer() {
 	return (
-		<div className="site-footer-wrapper">
+		<>
+      <div className="site-footer-wrapper">
 			<div className="footer-copyright">
 				<p className="text-center">© Movie List, 2023. All rights reserved</p>
 			</div>
 		</div>
+    <CookieConsent
+      buttonClasses="btn btn-sm"
+      buttonText="I agree"
+    >
+      <p className="m-0 text-center small">Cookies help us deliver the best experience on our website. By using our website, you agree to the use of cookies.</p>
+    </CookieConsent>
+    </>
 	);
 }
 
